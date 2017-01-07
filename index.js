918e2d04cc6197787f1886851e25c541ed2c61d0
Lukas Martinelli <me@lukasmartinelli.ch>
2017-01-07 11:33:27 +0100

    First toggle works

--- index.js
+++ index.js
@@ -1,27 +1,61 @@
 'use strict';
-var stylegen = require('./stylegen');
+var stylegen = require('./lib/stylegen');
+var LayerWatcher = require('./lib/layerwatcher');
 
-function InspectButton() {
+function InspectToggle(opts) {
+  var enabled = false;
+
   var btn = document.createElement("button");
   btn.className = "mapboxgl-ctrl-icon mapboxgl-ctrl-zoom-in"
   btn.type = 'button'
   btn['aria-label'] = 'Inspect'
   btn.onclick = function() {
+    enabled = !enabled;
+    opts.onToggle(enabled);
     console.log('Enable inspect')
   }
-  return btn
+
+  var container = document.createElement('div');
+  container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group';
+  container.appendChild(btn)
+  return container
 }
 
 function MapboxInspector(options) {
   if (!(this instanceof MapboxInspector)) {
     throw new Error("MapboxInspector needs to be called with the new keyword");
   }
-
   this.onAdd = function(map) {
+    console.log(stylegen);
+    var inspectStyle = stylegen.generateInspectStyle({
+      version: 8,
+      layers: [],
+      sources: []
+    }, []);
+
+    var watcher = new LayerWatcher({
+      onSourcesChange: function(sources) {
+        var coloredLayers = stylegen.generateColoredLayers(sources);
+        inspectStyle = stylegen.generateInspectStyle(map.getStyle(), coloredLayers);
+      }
+    });
+
+    //TODO: We need to unsubscribe on remove
+    map.on("data", function(e) {
+      if(e.dataType !== 'tile') return;
+      watcher.analyzeMap(map);
+    });
+
     this._map = map;
-    this._container = document.createElement('div');
-    this._container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group';
-    this._container.appendChild(InspectButton())
+    this._container = InspectToggle({
+      onToggle: function(enable) {
+        console.log('Toggle', enable)
+        if(enable) {
+          map.setStyle(inspectStyle);
+        } else {
+        }
+      }
+    });
     return this._container;
   };
 
